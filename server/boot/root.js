6106773ec35ed0b8973cd39deb2e30c3af3455b6
Oualid Amallah <walidamallah@gmail.com>
2017-11-03 09:15:08 +0100

    Añadir login a la vista prencipal

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,22 @@
 'use strict';
 
+var dsConfig = require('../datasources.local.js');
+var path = require('path');
+
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  //router.get('/', server.loopback.status());
+  //server.use(router);
+  var User = server.models.Usuario;
+
+   //login page
+   router.get('/', function(req, res) {
+    var credentials = dsConfig.myEmailDataSource.transports[0].auth;
+    res.render('login', {
+      email: credentials.user,
+      password: credentials.pass
+    });
+  });
   server.use(router);
 };
