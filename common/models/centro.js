ee499e8320b9fe37df4224e76988f28888cb9131
Oualid Amallah <walidamallah@gmail.com>
2017-11-13 07:26:00 +0100

    Vireficar Send Email al registrar centro

--- common/models/centro.js
+++ common/models/centro.js
@@ -3,4 +3,25 @@
 module.exports = function(Centro) {
 	Centro.validatesNumericalityOf('codigocentro', {int: true, message: 'Debe ser un número sin decimales'});
 	Centro.validatesUniquenessOf('codigocentro', {message: 'Ese centro ya existe'});
+	//var url = 'http://' + config.host + ':' + config.port + '/reset-password';
+	var html = 'Te Has REgistrado';
+
+	Centro.beforeRemote('create', function(context, centro, next) {
+		centro.verificado=false;
+	});
+
+	Centro.afterRemote('create', function(context, centro, next) {
+		    Centro.app.models.Email.send({
+			to: process.env.ADMIN_EMAIL,
+			from: process.env.EMAIL_USER,
+			subject: 'Alta de Centro',
+			html: html
+		  }, function(err) {
+			if (err) return console.log('> error al Enviado correctamente');
+			console.log('> Se Ha Enviado correctamente');
+		  });
+		  next();
+});
 };
+
+
